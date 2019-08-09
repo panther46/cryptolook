9521d67600f983501b7062552da47bd629a94710
sam <md346@hotmail.com>
2019-08-09 13:53:37 +0200

    version 1.01 Calling the API, using axios fixing form.

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,20 +1,41 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import axios from 'axios';
 
 
 function Form() {
+    // useEffect para llamar a la api
+    useEffect (() =>{
+        const consultarApi = async () =>{
+            const url = 'https://min-api.cryptocompare.com/data/top/totaltoptiervolfull?limit=10&tsym=USD';
+            const resultado = await axios.get(url);
+
+            console.log(resultado.data.Data);
+        }
+        // metodo, llamando. 
+        consultarApi();
+    }, []);
+
+
     return(
        <form>
-           <div class="form-group">
-            <label for="example">Choose your Currency</label>
-             <input type="email" class="form-control" id="example" aria-describedby="emailHelp" placeholder="Currency"></input>
-             <small id="emailHelp" class="form-text text-muted">Escoge la moneda que quieres consultar</small>
+           <div className="form-group">
+            <label htmlFor="example">Choose your Currency</label>
+            <select className="form-control" id="exampleFormControlSelect1">
+                <option value = "">Choose your Currency</option>
+                <option value = "USD">US Dollar</option>
+                <option value = "ES">Euro</option>
+                <option value = "GBP">Pounds</option>
+            </select>
+             <small id="emailHelp" className="form-text text-muted">Escoge la moneda que quieres consultar</small>
             </div>
-            <div class="form-group">
-            <label for="example">Choose your CryptoCurrency</label>
-             <input type="email" class="form-control" id="example" aria-describedby="Helop" placeholder="Currency"></input>
-             <small id="currencyHelp" class="form-text text-muted">Escoge la Cripto moneda</small>
+            <div className="form-group">
+            <label htmlFor="example">Choose your CryptoCurrency</label>
+             <select>
+                 
+             </select>
+             <small id="currencyHelp" className="form-text text-muted">Escoge la Cripto moneda</small>
             </div>
-            <button type="button" class="btn btn-primary btn-lg btn-block">CALCULATE</button>
+            <button type="button" className="btn btn-primary btn-lg btn-block">CALCULATE</button>
        </form>
 
     )
